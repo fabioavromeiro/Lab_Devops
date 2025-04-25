5a99bb710f1ed35a613380665b37e14eb5a880e7
fabioavromeiro <fabioavromeiro@gmail.com>
2025-04-25 07:11:44 -0300

    Nova home finalizada

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function mostraNome(nome){
     return nome
 }
+
+function mostraSobrenome(sobreNome){
+    return sobreNome;
+}
+
+//finalizado sistema da home
--- login.js
+++ login.js
@@ -1,3 +1,12 @@
 //trabalhando no login
 
 //finalizando sistema de login
+
+function login(nome, email){
+    return 'Bem vindo';
+}
+
+//seja bem vindo
+
+//Sistema de login completo
+//Retornamos o nome de usuario quando logado
